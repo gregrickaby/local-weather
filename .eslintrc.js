4f8c184e24ad045ad35f16ac28d4ccc79f25a581
Greg Rickaby <gregrickaby@gmail.com>
2022-01-10 15:49:23 -0600

    level up linting

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:jsx-a11y/recommended',
+    'next',
+    'next/core-web-vitals',
+    'prettier'
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'func-style': ['error', 'declaration'],
+    'jsx-a11y/anchor-is-valid': 'off',
+    'no-console': ['error', {allow: ['warn', 'error']}],
+    'prettier/prettier': 'error'
+  }
+}
