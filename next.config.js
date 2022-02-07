75a0ed2ef12cb931b4fe15c3f02ddd3592a0ebef
Greg Rickaby <gregrickaby@gmail.com>
2022-02-07 10:50:06 -0600

    define type

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['api.weather.gov', 'radar.weather.gov']
   }
 }
+
+module.exports = nextConfig
