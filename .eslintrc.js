68d8b18f306e7715ff3a1cdcf29c697807d564fa
Greg Rickaby <gregrickaby@gmail.com>
2022-02-07 10:49:45 -0600

    configure jsdoc

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,45 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
+    'plugin:jsdoc/recommended',
     'plugin:jsx-a11y/recommended',
     'next',
     'next/core-web-vitals',
     'prettier'
   ],
-  plugins: ['prettier'],
+  settings: {
+    jsdoc: {
+      mode: 'typescript',
+      tagNamePreference: {
+        returns: 'return'
+      }
+    }
+  },
+  plugins: ['prettier', 'jsdoc'],
   rules: {
-    'func-style': ['error', 'declaration'],
+    'jsdoc/check-indentation': 'warn',
+    'jsdoc/check-line-alignment': [
+      'warn',
+      'always',
+      {
+        tags: [
+          'param',
+          'return',
+          'property',
+          'type',
+          'default',
+          'name',
+          'description'
+        ]
+      }
+    ],
+    'jsdoc/require-param': [
+      'warn',
+      {
+        checkRestProperty: true,
+        unnamedRootBase: ['props']
+      }
+    ],
     'jsx-a11y/anchor-is-valid': 'off',
     'no-console': ['error', {allow: ['warn', 'error']}],
     'prettier/prettier': 'error'
