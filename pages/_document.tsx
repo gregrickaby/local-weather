e986f67d250623aad76fd29c51d609469e2bd7d1
Greg Rickaby <gregrickaby@gmail.com>
2022-02-07 11:56:49 -0600

    ts support

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,18 @@
-import Document, {Head, Html, Main, NextScript} from 'next/document'
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript
+} from 'next/document'
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
   render() {
     return (
       <Html lang="en">
