4b7f683899dafef4d57a87a3ef1c2d525a8d1cdc
Greg Rickaby <gregrickaby@gmail.com>
2022-02-09 21:55:13 -0600

    useForm. scaffold styles

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,5 +1,8 @@
+import {Button, Grid, TextInput} from '@mantine/core'
+import {useForm} from '@mantine/hooks'
 import {useState} from 'react'
 import {useSearchContext} from './SearchProvider'
+
 /**
  * Render the SearchBar component.
  *
@@ -9,30 +12,52 @@ import {useSearchContext} from './SearchProvider'
 export default function SearchBar() {
   const search = useSearchContext()
   const [value, setValue] = useState('Bay Lake, FL')
+  const form = useForm<{search?: string}>({
+    initialValues: {
+      search: 'Bay Lake, FL'
+    },
 
-  /**
-   * Search handler.
-   *
-   * @param {object} event The event object.
-   */
-  function handleSearch(event) {
-    event.preventDefault()
-    search.setSearch(value)
-  }
+    validationRules: {
+      search: (value) => value.trim().length >= 2
+    }
+  })
 
   return (
-    <form onSubmit={handleSearch}>
-      <label htmlFor="search">Enter your location</label>
-      <input
-        id="search"
-        minLength={4}
-        onChange={(e) => setValue(e.target.value)}
-        pattern="^[^~`^<>]+$"
-        placeholder="Bay Lake, FL"
-        type="text"
-        value={value}
-      />
-      <button>Search</button>
+    <form
+      onSubmit={form.onSubmit((values) => {
+        search.setSearch(value)
+      })}
+    >
+      <Grid gutter="md">
+        <Grid.Col span={8}>
+          <TextInput
+            sx={{
+              label: {
+                borderWidth: 0,
+                clip: 'rect(0, 0, 0, 0)',
+                height: '1px',
+                margin: '-1px',
+                overflow: 'hidden',
+                padding: 0,
+                position: 'absolute',
+                whiteSpace: 'nowrap',
+                width: '1px'
+              }
+            }}
+            label="Enter your location"
+            id="search"
+            minLength={4}
+            onChange={(e) => setValue(e.target.value)}
+            pattern="^[^~`^<>]+$"
+            placeholder="Bay Lake, FL"
+            type="text"
+            value={value}
+          />
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Button type="submit">Search</Button>
+        </Grid.Col>
+      </Grid>
     </form>
   )
 }
