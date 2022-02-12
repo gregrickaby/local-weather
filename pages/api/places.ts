eb203206ee349c6574a082bd487efe8280c9b985
Greg Rickaby <gregrickaby@gmail.com>
2022-02-11 22:14:19 -0600

    update jsdocs. return array

--- pages/api/places.ts
+++ pages/api/places.ts
@@ -1,16 +1,16 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 
 /**
- * Geocode the address into latitude and longitude coordinates.
+ * Predict the city via Google's Places Autocomplete API.
  *
  * @example
- * /api/geocoding?address="orlando,+fl"
+ * /api/places?city="orlando,+fl"
  *
  * Note: In Google Cloud Platform --> Credentials, you must set
- * application restrictions to "None" and then "Restrict key" to "Geocoding".
+ * application restrictions to "None" and then "Restrict key" to "Places".
  * @author Greg Rickaby
  * @see https://console.cloud.google.com/apis/credentials
- * @see https://developers.google.com/maps/documentation/geocoding/overview
+ * @see https://developers.google.com/maps/documentation/places/web-service/autocomplete
  * @see https://nextjs.org/docs/api-routes/introduction
  * @see https://nodejs.org/api/http.html#http_class_http_incomingmessage
  * @see https://nodejs.org/api/http.html#http_class_http_serverresponse
@@ -32,8 +32,13 @@ export default async function places(
 
     // If the response is "OK", continue.
     if (data.status === 'OK') {
+      // Build the list of cities.
+      const cities = data?.predictions.map((prediction: any) => {
+        return prediction?.description
+      })
+
       // Return the predictions.
-      res.status(200).json(data)
+      res.status(200).json(cities)
 
       // Otherwise, return an error.
     } else {
