71c302c527a7db1677184a5e37e15bd522fac45a
Greg Rickaby <gregrickaby@gmail.com>
2022-02-11 22:14:52 -0600

    initial commit of autocomplete

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,25 +1,8 @@
-import {Button, createStyles, TextInput} from '@mantine/core'
-import {useForm} from '@mantine/hooks'
+import {Autocomplete} from '@mantine/core'
+import {SewingPinFilledIcon} from '@modulz/radix-icons'
 import {useState} from 'react'
+import usePlaces from '~/lib/usePlaces'
 import {useSearchContext} from './SearchProvider'
-
-const useStyles = createStyles((theme, _params, getRef) => {
-  const button = getRef('button')
-
-  return {
-    form: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    button: {
-      ref: button,
-      borderTopLeftRadius: 0,
-      borderLeft: 0,
-      borderBottomLeftRadius: 0
-    }
-  }
-})
 
 /**
  * Render the SearchBar component.
@@ -29,47 +12,41 @@ const useStyles = createStyles((theme, _params, getRef) => {
  */
 export default function SearchBar() {
   const search = useSearchContext()
-  const {classes} = useStyles()
   const [value, setValue] = useState('Bay Lake, FL')
-  const form = useForm<{search?: string}>({
-    initialValues: {
-      search: 'Bay Lake, FL'
-    },
+  const {cities} = usePlaces(false, value)
 
-    validationRules: {
-      search: (value) => value.trim().length >= 2
-    }
-  })
+  const locations =
+    !!cities && cities.length > 0
+      ? cities
+      : [
+          'New York, NY',
+          'Los Angeles, CA',
+          'Chicago, IL',
+          'Houston, TX',
+          'Phoenix, AZ',
+          'Philadelphia, PA',
+          'San Antonio, TX',
+          'San Diego, CA',
+          'Dallas, TX',
+          'San Jose, CA'
+        ]
 
   return (
-    <form
-      className={classes.form}
-      onSubmit={form.onSubmit((values) => {
+    <Autocomplete
+      aria-label="Search for your location"
+      data={locations}
+      icon={<SewingPinFilledIcon />}
+      label="Search"
+      onChange={setValue}
+      onItemSubmit={() => {
         search.setSearch(value)
-      })}
-    >
-      <TextInput
-        id="search"
-        aria-label="Enter your location"
-        minLength={4}
-        onChange={(e) => setValue(e.target.value)}
-        pattern="^[^~`^<>]+$"
-        placeholder="Bay Lake, FL"
-        size="md"
-        styles={{
-          input: {
-            borderTopRightRadius: 0,
-            borderBottomRightRadius: 0,
-            borderRight: 0
-          }
-        }}
-        type="text"
-        value={value}
-      />
-
-      <Button className={classes.button} type="submit" size="md">
-        Search
-      </Button>
-    </form>
+      }}
+      limit={10}
+      placeholder="Search for a city"
+      transition="pop-top-left"
+      transitionDuration={80}
+      transitionTimingFunction="ease"
+      value={value}
+    />
   )
 }
