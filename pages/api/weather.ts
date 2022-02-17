3d9870ce67f5470beaf59c89d500c79f876672ff
Greg Rickaby <gregrickaby@gmail.com>
2022-02-17 12:31:47 -0600

    use openweather api

--- pages/api/weather.ts
+++ pages/api/weather.ts
@@ -1,14 +1,14 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 
 /**
- * Fetch weather data from the National Weather Service.
+ * Fetch weather data from the OpenWeather API.
  *
  * @example
  * /api/weather?location=bay lake,+fl
  * @author Greg Rickaby
  * @see https://console.cloud.google.com/apis/credentials
  * @see https://developers.google.com/maps/documentation/geocoding/overview
- * @see https://weather-gov.github.io/api/general-faqs
+ * @see https://openweathermap.org/api/one-call-api
  * @see https://nextjs.org/docs/api-routes/introduction
  * @see https://nodejs.org/api/http.html#http_class_http_incomingmessage
  * @see https://nodejs.org/api/http.html#http_class_http_serverresponse
@@ -24,7 +24,7 @@ export default async function weather(
 
   // Set default coordinates as fallback.
   let lat: number = 28.3886186
-  let lng: number = -81.5659069
+  let lon: number = -81.5659069
 
   // No address? Bail...
   if (!location) {
@@ -44,7 +44,7 @@ export default async function weather(
     if (coordinates.status === 'OK') {
       // Pluck out the coordinates.
       lat = coordinates?.results[0]?.geometry?.location?.lat
-      lng = coordinates?.results[0]?.geometry?.location?.lng
+      lon = coordinates?.results[0]?.geometry?.location?.lng
 
       // Otherwise, return an error.
     } else {
@@ -58,40 +58,11 @@ export default async function weather(
   }
 
   try {
-    // NWS requires two requests.
-    // The first request is to get the forecast point based on geocoding.
-    const requestOne = await fetch(
-      `https://api.weather.gov/points/${lat},${lng}`
+    const weather = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely&appid=${process.env.OPENWEATHER_API_KEY}`
     )
-    const point = await requestOne.json()
-
-    // The second request is to get the actual forecast.
-    const requestTwo = await fetch(point.properties.forecast)
-    const forecast = await requestTwo.json()
-
-    // Third request is to get the alerts.
-    const requestThree = await fetch(
-      `https://api.weather.gov/alerts/active?status=actual&message_type=alert&point=${lat},${lng}`
-    )
-    const alerts = await requestThree.json()
-
-    // Four request is to get the local weather office.
-    const requestFour = await fetch(point.properties.forecastOffice)
-    const station = await requestFour.json()
-
     // Send the response.
-    res.status(200).json({
-      alerts: alerts?.features,
-      coordinates: {lat, lng},
-      forecast: forecast?.properties?.periods,
-      location: {
-        city: point?.properties?.relativeLocation?.properties?.city,
-        state: point?.properties?.relativeLocation?.properties?.state
-      },
-      radar: point?.properties?.radarStation,
-      station: station?.name,
-      updated: forecast?.properties?.updated
-    })
+    res.status(200).json(await weather.json())
   } catch (error) {
     // Issue? Leave a message and bail.
     res.status(500).json({message: `${error}`})
