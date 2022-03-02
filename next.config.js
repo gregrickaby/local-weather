593f4099e697e72a804d2dc9bf57ff1cf58fd6af
Greg Rickaby <gregrickaby@gmail.com>
2022-03-01 21:43:37 -0600

    remove image config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['api.weather.gov', 'radar.weather.gov', 'openweathermap.org'],
-    formats: ['image/avif', 'image/webp']
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
