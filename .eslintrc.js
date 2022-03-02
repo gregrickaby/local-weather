58d36bc2f89b50cd53dc895fba6590b68040345f
Greg Rickaby <gregrickaby@gmail.com>
2022-03-01 21:43:12 -0600

    remove jsdocs

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,45 +6,13 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:jsdoc/recommended',
     'plugin:jsx-a11y/recommended',
     'next',
     'next/core-web-vitals',
     'prettier'
   ],
-  settings: {
-    jsdoc: {
-      mode: 'typescript',
-      tagNamePreference: {
-        returns: 'return'
-      }
-    }
-  },
-  plugins: ['prettier', 'jsdoc'],
+  plugins: ['prettier'],
   rules: {
-    'jsdoc/check-indentation': 'warn',
-    'jsdoc/check-line-alignment': [
-      'warn',
-      'always',
-      {
-        tags: [
-          'param',
-          'return',
-          'property',
-          'type',
-          'default',
-          'name',
-          'description'
-        ]
-      }
-    ],
-    'jsdoc/require-param': [
-      'warn',
-      {
-        checkRestProperty: true,
-        unnamedRootBase: ['props']
-      }
-    ],
     'no-unused-vars': 'off',
     'jsx-a11y/anchor-is-valid': 'off',
     'no-console': ['error', {allow: ['warn', 'error']}],
