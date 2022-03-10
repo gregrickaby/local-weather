621d20eda085da2b8c5b9679dce0b26eb2cc968b
Greg Rickaby <gregrickaby@gmail.com>
2022-03-10 16:42:53 -0600

    refactor

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,28 +1,29 @@
 import {Center, Text} from '@mantine/core'
+import {GitHubLogoIcon} from '@modulz/radix-icons'
 
 export default function Footer() {
   return (
     <footer>
       <Center>
-        <Text m="xl" size="sm">
-          Weather data from{' '}
-          <Text
-            component="a"
-            href="https://openweathermap.org/api"
-            size="sm"
-            variant="link"
-          >
-            OpenWeatherAPI
-          </Text>{' '}
-          | Website by{' '}
+        <Text m="xl" size="sm" sx={{fontFamily: 'monospace'}}>
+          website by{' '}
           <Text
             component="a"
             href="https://gregrickaby.com"
+            rel="author"
             size="sm"
             variant="link"
-            rel="author"
+            sx={{fontFamily: 'monospace'}}
           >
-            Greg Rickaby
+            @gregrickaby
+          </Text>{' '}
+          <Text
+            aria-label="View source code on GitHub"
+            component="a"
+            href="https://github.com/gregrickaby/local-weather"
+            sx={{verticalAlign: 'sub'}}
+          >
+            <GitHubLogoIcon />
           </Text>
         </Text>
       </Center>
