fa195d7015df37da0ac3a3d00acc08854be99864
Greg Rickaby <gregrickaby@gmail.com>
2022-03-10 17:04:14 -0600

    use location in title

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -1,10 +1,17 @@
 import Head from 'next/head'
+import {useWeatherContext} from './WeatherProvider'
 
 export default function Meta() {
+  const {location} = useWeatherContext()
   return (
     <Head>
-      <title>Local Weather</title>
-      <meta name="description" content="View the local weather forecast." />
+      <title>{location} - Local Weather</title>
+      <meta
+        name="description"
+        content="View the current weather conditions, long-range forecast ,and get weather alerts for your local area."
+      />
+      <link rel="preconnect" href="//api.openweathermap.org" />
+      <link rel="preconnect" href="//maps.googleapis.com" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   )
