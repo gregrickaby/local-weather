f088c94f05ab57c43f52b2084e627904de19223a
Greg Rickaby <gregrickaby@gmail.com>
2022-06-17 17:46:50 -0500

    remove location

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -1,11 +1,9 @@
 import Head from 'next/head'
-import {useWeatherContext} from '~/components/WeatherProvider'
 
 export default function Meta() {
-  const {location} = useWeatherContext()
   return (
     <Head>
-      <title>{location} - Local Weather</title>
+      <title>Local Weather</title>
       <meta
         name="description"
         content="View the current weather conditions, long-range forecast, and get weather alerts for your local area."
