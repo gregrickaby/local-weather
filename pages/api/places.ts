97ea9ec6ab17d031367706df8146a6f1ac59a38d
Greg Rickaby <gregrickaby@gmail.com>
2022-09-21 12:16:38 -0500

    add statusText. additional check on initial response.

--- pages/api/places.ts
+++ pages/api/places.ts
@@ -32,44 +32,61 @@ export default async function places(req: NextRequest) {
   // No location? Bail...
   if (!location) {
     return new Response(JSON.stringify({error: 'No location provided.'}), {
-      status: 400
+      status: 400,
+      statusText: 'Bad Request'
     })
   }
 
   try {
     // Attempt to fetch the city from Google's Places API.
-    const response = await fetch(
+    const places = await fetch(
       `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${location}&types=(cities)&language=en&key=${process.env.GOOGLE_MAPS_API_KEY}`
     )
 
-    // Parse the response.
-    const data = (await response.json()) as Place
-
-    // Issue with the response? Bail...
-    if (data.status != 'OK' || !data.predictions.length) {
+    // Issue with the places response? Bail...
+    if (places.status != 200) {
       return new Response(
         JSON.stringify({
-          error: `${data.status}`
+          error: `${places.statusText}`
         }),
         {
-          status: 400
+          status: places.status,
+          statusText: places.statusText
         }
       )
     }
 
-    // Build the list of places.
-    const places = data.predictions.map(
+    // Parse the response.
+    const place = (await places.json()) as Place
+
+    // Issue with the response? Bail...
+    if (place.status != 'OK' || !place.predictions.length) {
+      return new Response(
+        JSON.stringify({
+          error: place.status
+        }),
+        {
+          status: 400,
+          statusText: 'Bad Request'
+        }
+      )
+    }
+
+    // Build the list of locations.
+    const locations = place.predictions.map(
       (prediction: PredictionResponse) => prediction.description
     ) as string[]
 
-    // Return the list of places.
-    return new Response(JSON.stringify(places), {
-      status: 200
+    // Return the list of locations.
+    return new Response(JSON.stringify(locations), {
+      status: 200,
+      statusText: 'OK'
     })
   } catch (error) {
     console.error(error)
     return new Response(JSON.stringify({error: `${error}`}), {
-      status: 500
+      status: 500,
+      statusText: 'Internal Server Error'
     })
   }
 }
