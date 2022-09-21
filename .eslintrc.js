173aa08731a40e5902e33b27498d075657cd1d71
Greg Rickaby <gregrickaby@gmail.com>
2022-09-21 13:33:20 -0500

    use minimal eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,6 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  env: {
-    browser: true,
-    node: true,
-    es6: true
-  },
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'eslint:recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'next/core-web-vitals',
-    'prettier'
-  ],
+  extends: ['next/core-web-vitals', 'prettier'],
   rules: {
-    'no-unused-vars': 'off',
-    'jsx-a11y/anchor-is-valid': 'off',
-    'no-console': ['error', {allow: ['warn', 'error']}],
-    'prettier/prettier': 'error'
+    'no-console': ['error', {allow: ['warn', 'error']}]
   }
 }
