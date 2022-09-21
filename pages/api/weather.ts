7f421d84de3118fb07e03cea59aba941e7f72ea4
Greg Rickaby <gregrickaby@gmail.com>
2022-09-21 12:16:51 -0500

    refactor to use new Edge API route

--- pages/api/weather.ts
+++ pages/api/weather.ts
@@ -1,4 +1,23 @@
-import type {NextApiRequest, NextApiResponse} from 'next'
+import type {NextRequest} from 'next/server'
+import {WeatherResponse} from '~/lib/types'
+
+export const config = {
+  runtime: 'experimental-edge'
+}
+
+export interface GeocodeResponse {
+  status: string
+  results: [
+    {
+      geometry: {
+        location: {
+          lat: number
+          lng: number
+        }
+      }
+    }
+  ]
+}
 
 /**
  * Fetch weather data from the OpenWeatherMap API.
@@ -10,64 +29,116 @@ import type {NextApiRequest, NextApiResponse} from 'next'
  * @see https://console.cloud.google.com/apis/credentials
  * @see https://developers.google.com/maps/documentation/geocoding/overview
  * @see https://openweathermap.org/api/one-call-api
- * @see https://nextjs.org/docs/api-routes/introduction
- * @see https://nodejs.org/api/http.html#http_class_http_incomingmessage
- * @see https://nodejs.org/api/http.html#http_class_http_serverresponse
+ * @see https://nextjs.org/docs/api-routes/edge-api-routes
+ * @see https://nextjs.org/docs/api-reference/edge-runtime
  */
-export default async function weather(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function weather(req: NextRequest) {
+  // Get the location from the query string.
+  const location = new URL(req.url).searchParams.get('location')
+
+  // No location? Bail...
+  if (!location) {
+    return new Response(JSON.stringify({error: 'No location provided.'}), {
+      status: 400,
+      statusText: 'Bad Request'
+    })
+  }
+
   // Set default coordinates as fallback.
   let lat = 28.3886186
   let lon = -81.5659069
 
-  // No address? Bail...
-  if (!req.query.location) {
-    return res.status(400).json({
-      message: 'Cannot fetch weather information without a location query.'
+  try {
+    // First, try to geocode the address.
+    const geocode = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.GOOGLE_MAPS_API_KEY}`
+    )
+
+    // Issue with the geocode request? Bail...
+    if (geocode.status !== 200) {
+      return new Response(
+        JSON.stringify({
+          error: `${geocode.statusText}`
+        }),
+        {
+          status: geocode.status,
+          statusText: geocode.statusText
+        }
+      )
+    }
+
+    // Parse the response.
+    const coordinates = (await geocode.json()) as GeocodeResponse
+
+    // Issue with the response? Bail...
+    if (coordinates.status != 'OK' || !coordinates.results.length) {
+      return new Response(
+        JSON.stringify({
+          error: `${coordinates.status}`
+        }),
+        {
+          status: 400,
+          statusText: 'Bad Request'
+        }
+      )
+    }
+
+    // Pluck out and set the coordinates.
+    lat = coordinates?.results[0]?.geometry?.location?.lat
+    lon = coordinates?.results[0]?.geometry?.location?.lng
+  } catch (error) {
+    console.error(error)
+    return new Response(JSON.stringify({error: `${error}`}), {
+      status: 500,
+      statusText: 'Internal Server Error'
     })
   }
 
-  // First, try to geocode the address.
   try {
-    const geocode = await fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${req?.query?.location}&key=${process.env.GOOGLE_MAPS_API_KEY}`
-    )
-    const coordinates = await geocode.json()
-
-    // If the response is "OK", continue.
-    if (coordinates.status === 'OK') {
-      // Pluck out the coordinates.
-      lat = coordinates?.results[0]?.geometry?.location?.lat
-      lon = coordinates?.results[0]?.geometry?.location?.lng
-
-      // Otherwise, return an error.
-    } else {
-      return res.status(500).json({message: coordinates?.status})
-    }
-  } catch (error) {
-    // Other issue? Leave a message and bail.
-    return res.status(500).json({message: error})
-  }
-
-  // Now, fetch the weather data.
-  try {
+    // Now, fetch the weather data.
     const weather = await fetch(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely&appid=${process.env.OPENWEATHER_API_KEY}`
     )
-    const forecast = await weather.json()
 
-    // If there's a forecast, return it.
-    if (Object.keys(forecast).length) {
-      return res.status(200).json(forecast)
-
-      // Otherwise, return an error.
-    } else {
-      return res.status(500).json({message: 'Cannot fetch weather data.'})
+    // Issue with the weather response? Bail...
+    if (weather.status != 200) {
+      return new Response(
+        JSON.stringify({
+          error: `${weather.statusText}`
+        }),
+        {
+          status: weather.status,
+          statusText: weather.statusText
+        }
+      )
     }
+
+    // Parse the response.
+    const forecast = (await weather.json()) as WeatherResponse
+
+    // Issue with the forecast? Bail...
+    if (!forecast.lat || !forecast.lon) {
+      return new Response(
+        JSON.stringify({
+          error: 'No forecast data.'
+        }),
+        {
+          status: 400,
+          statusText: 'Bad Request'
+        }
+      )
+    }
+
+    // Return the weather data.
+    return new Response(JSON.stringify(forecast), {
+      status: 200,
+      statusText: 'OK'
+    })
   } catch (error) {
-    // Other issue? Leave a message and bail.
-    return res.status(500).json({message: error})
+    console.error(error)
+    return new Response(JSON.stringify({error: `${error}`}), {
+      status: 500,
+      statusText: 'Internal Server Error'
+    })
   }
 }
