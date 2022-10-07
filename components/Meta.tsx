02b0dc00d89391aff5b2c969da49cfa12249ac4b
Greg Rickaby <gregrickaby@gmail.com>
2022-10-07 14:06:17 -0500

    preload fetch requests

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 
+/**
+ * Meta component.
+ */
 export default function Meta() {
   return (
     <Head>
@@ -8,8 +11,18 @@ export default function Meta() {
         name="description"
         content="View the current weather conditions, long-range forecast, and get weather alerts for your local area."
       />
-      <link rel="preconnect" href="//api.openweathermap.org" />
-      <link rel="preconnect" href="//maps.googleapis.com" />
+      <link
+        as="fetch"
+        rel="preload"
+        href="/api/places?location=Enterprise,%20AL"
+        crossOrigin="same-origin"
+      />
+      <link
+        as="fetch"
+        rel="preload"
+        href="/api/weather?location=Enterprise,%20AL"
+        crossOrigin="same-origin"
+      />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   )
