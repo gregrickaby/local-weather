8091608fc0d04584148f0aca4da18da31787f740
Greg Rickaby <gregrickaby@gmail.com>
2023-10-12 15:27:36 -0500

    clean up header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,5 @@
 import {Title} from '@mantine/core'
 import classes from '~/components/Header.module.css'
-import Meta from '~/components/Meta'
-import Search from '~/components/Search'
 import config from '~/lib/config'
 
 /**
@@ -10,12 +8,10 @@ import config from '~/lib/config'
 export default function Header() {
   return (
     <>
-      <Meta />
       <header className={classes.header}>
         <Title className={classes.title} order={1}>
           {config.siteName}
         </Title>
-        <Search />
       </header>
     </>
   )
