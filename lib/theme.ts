1bab108f067c87f99e2bc2d9b0c180f09570ecb6
Greg Rickaby <gregrickaby@gmail.com>
2024-02-01 08:30:44 -0600

    move them to /lib

---
+++ lib/theme.ts
@@ -0,0 +1,14 @@
+'use client'
+
+import {createTheme} from '@mantine/core'
+
+/**
+ * Customize Mantine Theme.
+ *
+ * @see https://mantine.dev/theming/mantine-provider/#theme
+ */
+const theme = createTheme({
+  primaryColor: 'gray'
+})
+
+export default theme
