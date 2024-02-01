9126708f2908a0c7c799130b85dad8852cf3ff9c
Greg Rickaby <gregrickaby@gmail.com>
2024-02-01 08:30:20 -0600

    remove old settings. add logging

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   images: {
     formats: ['image/avif', 'image/webp']
+  },
+  logging: {
+    fetches: {
+      fullUrl: true
+    }
   }
 }
 
