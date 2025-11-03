7694f45e0afb5f586cd5b3703253bab6a87b1734
Greg Rickaby <gregrickaby@gmail.com>
2025-11-03 09:24:07 -0600

    fix up footer

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import config from '@/lib/constants/config'
+import {Text} from '@mantine/core'
 import classes from './Footer.module.css'
 
 /**
@@ -7,46 +8,47 @@ import classes from './Footer.module.css'
 export default function Footer() {
   return (
     <footer className={classes.footer}>
-      <div className={classes.content}>
-        <p>
-          Weather data from{' '}
-          <a
-            aria-label="visit Open-Meteo website"
-            href="https://open-meteo.com/"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Open-Meteo
-          </a>
-          . Radar data from{' '}
-          <a
-            aria-label="visit RainViewer website"
-            href="https://www.rainviewer.com/"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            RainViewer
-          </a>
-          . Weather app created by{' '}
-          <a
-            aria-label={`visit ${config.siteAuthor} website`}
-            href={config.authorUrl}
-            rel="author"
-          >
-            {config.siteAuthor}
-          </a>
-          . View source on{' '}
-          <a
-            aria-label="view source code on github"
-            href={config.githubUrl}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            GitHub
-          </a>
-          {/* */}.
-        </p>
-      </div>
+      <Text>
+        &copy; {new Date().getFullYear()} {config.siteName} by{' '}
+        <a
+          aria-label={`visit ${config.siteAuthor} website`}
+          href={config.authorUrl}
+          rel="author"
+        >
+          {config.siteAuthor}
+        </a>
+      </Text>
+      <Text>
+        {' '}
+        Data from{' '}
+        <a
+          aria-label="visit Open-Meteo website"
+          href="https://open-meteo.com/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Open-Meteo
+        </a>{' '}
+        and{' '}
+        <a
+          aria-label="visit RainViewer website"
+          href="https://www.rainviewer.com/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          RainViewer
+        </a>
+        . View source code on{' '}
+        <a
+          aria-label="view source code on github"
+          href={config.githubUrl}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          GitHub
+        </a>
+        {/* */}.
+      </Text>
     </footer>
   )
 }
